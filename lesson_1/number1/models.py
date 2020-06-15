50ee8ec428027db9df118117a7adffe97144c945
Vitaliy <v.sadetdinov@yandex.ru>
2020-06-15 15:19:11 +0300

    Создал модели User, Blog, Post. Запустил админку

--- lesson_1/number1/models.py
+++ lesson_1/number1/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField(max_length=2)
+
+
+class Blog(models.Model):
+    start_date = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    topic = models.CharField(max_length=30, null=True)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+    body = models.TextField(blank=True, db_index=True)
+    date_pub = models.DateField(auto_now_add=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, null=True)
+    topic = models.CharField(max_length=30, null=True)
